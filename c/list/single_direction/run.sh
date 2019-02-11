359ffa46e8426476a2aedcdc15bc510752a86e37
yosukesan <y.otsuki30@gmail.com>
2019-02-11 21:42:13 +0900

    some debug options to CMAKE_C_FLAGS_DEBUG and CMAKE_CXX_FLAGS_DEBUG were added.

--- c/list/single_direction/run.sh
+++ c/list/single_direction/run.sh
@@ -8,7 +8,9 @@ fi
 cd build
 
 # configure 
-cmake -DCMAKE_INSTALL_PREFIX=../${PROJECT} -DCMAKE_BUILD_TYPE=Debug ../
+cmake -DCMAKE_INSTALL_PREFIX=../${PROJECT} \
+	-DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS_DEBUG="-g -Wall -W -pedantic -O0" \
+	 -DCMAKE_C_FLAGS_DEBUG="-g -Wall -W -pedantic -O0" ../
 
 # build
 make -j 2
