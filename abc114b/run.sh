d4ea215f420b998334d54440f3fc9d7f6c00544a
yosukesan <y.otsuki30@gmail.com>
2019-04-17 00:19:18 +0900

    rm at_coders in the root dir. They were duplicated.

--- abc114b/run.sh
+++
@@ -1,4 +0,0 @@
-
-echo 1234567876 | python3 main.py
-echo 35753 | python3 main.py
-echo 1111111111 | python3 main.py
--- abc117a/run.sh
+++
@@ -1,4 +0,0 @@
-
-echo "8 3" | ./a.out
-echo "99 1" | ./a.out
-echo "1 100" | ./a.out
