a32cebe187189d067f6c4ebc519bac85dc399621
yosukesan <y.otsuki30@gmail.com>
2019-04-23 00:21:10 +0900

    restructure

--- 20190420_kenta1_beginner/b/run.sh
+++
@@ -1,4 +0,0 @@
-
-echo "5 error 2" | ./a.out
-echo "6 eleven 5" | ./a.out
-echo "9 education 7" | ./a.out
--- 20190420_kenta1_beginner/c/run.sh
+++
@@ -1,5 +0,0 @@
-
-echo "3 #.#" | ./a.out
-echo "5 #.##." | ./a.out
-echo "9 #.##.##.." | ./a.out
-echo "9 ........." | ./a.out
