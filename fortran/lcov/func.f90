2176fc1f250a7116f05a24ff24531f9f746b4d6d
yosukesan <y.otsuki30@gmail.com>
2019-05-04 00:12:16 +0900

    add lcov sample with result

---
+++ fortran/lcov/func.f90
@@ -0,0 +1,12 @@
+
+subroutine func(a)
+  implicit none
+  integer :: a
+
+  if (a .ne. 0) then
+    write(*,*) a
+  else
+    write(*,*) a
+  end if
+
+end subroutine func
---
+++ fortran/lcov/main.f90
@@ -0,0 +1,8 @@
+
+program main
+  implicit none
+
+  call func(0)
+  call func(1)
+
+end program main
