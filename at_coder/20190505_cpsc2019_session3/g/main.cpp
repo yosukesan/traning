d6a0d93106414ddb5aaeedf99e7ac4925a0bf98f
yosukesan <y.otsuki30@gmail.com>
2019-05-05 15:57:59 +0900

    on the way

---
+++ at_coder/20190505_cpsc2019_session3/g/main.cpp
@@ -0,0 +1,40 @@
+
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    long long n, X;
+    long long a[100010];
+    double x[100010];
+    double delta = 0;
+    long long denm = 0;
+
+    cin >> n >> X;
+
+    for (long long i=0; i<n; ++i)
+        cin >> a[i];
+
+    for (long long i=0; i<n; ++i)
+        denm += a[i];
+
+    for (long long i=0; i<n; ++i)
+        x[i] = round(X * (1.0 / denm) * a[i]);
+
+    for (long long i=0; i<n; ++i)
+        delta = x[i] - X * (1.0 / denm); 
+
+    if (!(delta >= 1.0))
+    {
+        sort(x, x+n, greater<int>());
+        x[n-1]++;
+    }
+
+    for (long long i=0; i<n; ++i)
+        cout << x[i] << endl;
+
+    return 0;
+}
---
+++ at_coder/20190505_cpsc2019_session3/g/run.sh
@@ -0,0 +1,6 @@
+
+echo "4 100 3 4 5 8" | ./a.out
+echo
+echo "2 100 3 4" | ./a.out
+echo
+echo "6 1000 7 6 1 3 4 1" | ./a.out
