2bdb80dc70a6f89d8995690245843919dff9ae5c
Yosuke <y.otsuki30@gmail.com>
2020-01-30 01:55:08 +0900

    abc118 c solved

--- at_coder/abc118/c/main.cpp
+++ at_coder/abc118/c/main.cpp
@@ -1,27 +1,44 @@
 
-#include <vector>
 #include <iostream>
+#include <string>
 #include <algorithm>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+
+using ll = long long int;
+using ul = unsigned long long int;
+const ll LL_MAX (1LL<<60);
+const ll MOD (1000000007);
+
+#define rep(i,s,e) for(ll i=(s); i<(e); i++) 
 
 using namespace std;
 
+ll gcd (ll a, ll b)
+{
+    return b ? gcd (b, a%b) : a;
+}
+
 int main()
 {
-	int num, tmp;
+    cin.tie(0);
+    ios::sync_with_stdio(false);
 
-	cin >> num;
-    vector<int> a;
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i,0,n) cin >> a[i];
 
-	for (int i=0; i<num; ++i)
-	{
-		cin >> tmp;
-        a.push_back(tmp);
-	}
+    ll ans(LL_MAX);
+    rep(i,1,n) ans = min(ans, gcd(a[i], a[i-1])); 
 
-	for (int i=0; i<num; ++i)
-	{
-        cout << a[i] << endl;
-    }
+    cout << ans << endl;
 
-	return 0;
+    return 0;
 }
