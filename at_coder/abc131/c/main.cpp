037c73a9c7474113222ac03f720e257189a370b0
Yosuke <y.otsuki30@gmail.com>
2020-01-30 22:40:42 +0900

    abc131 c solved

--- at_coder/abc131/c/main.cpp
+++ at_coder/abc131/c/main.cpp
@@ -10,12 +10,34 @@ typedef long long ll;
 
 using namespace std;
 
+ll gcd (ll a, ll b)
+{
+    return b ? gcd(b, a%b) : a;
+}
+
+ll lcm (ll a, ll b)
+{
+    return a*b / gcd(a, b);
+}
+
+ll f (ll x, ll c, ll d)
+{
+    ll res = x;
+    res -= x/c;
+    res -= x/d;
+    res += x/lcm(c,d);
+    return res;
+}
+
 int main()
 {
     ll a,b,c,d;
     vector<ll> nums;
     cin >> a >> b >> c >> d;
 
+    ll cnt(f(b,c,d) - f(a-1, c, d));
+
+    cout << cnt << endl;
 
     return 0;
 }
