2103eadef530336fd39f5e1c291ff4eef0d43f6a
Yosuke <y.otsuki30@gmail.com>
2020-02-03 23:09:51 +0900

    abc080 b solved

---
+++ at_coder/abc080/b/main.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+
+using ll = long long int;
+using ul = unsigned long long int;
+const ll LL_MAX (1LL<<60);
+const ll MOD (1000000007);
+
+#define rep(i,s,e) for(ll i=(s); i<(e); i++) 
+
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    string n;
+    cin >> n;
+
+    ll s(0);
+    for (auto c: n)
+        s += c-'0';
+
+    int p = atoi(n.c_str());
+    if (p % s == 0)
+        cout << "Yes" << endl; 
+    else
+        cout << "No" << endl; 
+    
+
+    return 0;
+}
---
+++ at_coder/abc080/b/run.sh
@@ -0,0 +1,5 @@
+
+#echo "" | ./a.out
+echo "12" | ./a.out
+echo "57" | ./a.out
+echo "148" | ./a.out
