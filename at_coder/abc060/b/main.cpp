39a25aefed8a8cfef24889028398369b0bfc0a7e
Yosuke <y.otsuki30@gmail.com>
2020-02-17 01:20:15 +0900

    abc060 a and b solved

---
+++ at_coder/abc060/b/main.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+
+using ll = long long int;
+using ul = unsigned long long int;
+const ll LL_MAX (1LL<<60);
+const ll MOD (1000000007);
+
+#define rep(i,s,e) for(ll i=(s); i<(e); i++) 
+
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    ll a, b, c;
+    cin >> a >> b >> c;
+
+    bool ans(false);
+    for (int i=1; i<100; i++)
+    {
+        ll res = a*i % b;
+        ans = (res == c);
+        if (ans) break;
+    }
+
+    if (ans)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
---
+++ at_coder/abc060/b/run.sh
@@ -0,0 +1,7 @@
+
+#echo "" | ./a.out
+echo "7 5 1" | ./a.out
+echo "2 2 1" | ./a.out
+echo "1 100 97" | ./a.out
+echo "40 98 58" | ./a.out
+echo "77 42 36" | ./a.out
