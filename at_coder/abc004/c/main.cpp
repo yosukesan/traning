b19e0460c63a1f8d6a0b05edfd6132bb68c48b5c
Yosuke <y.otsuki30@gmail.com>
2020-06-28 12:00:23 +0900

    solved abc004/c brutal method

---
+++ at_coder/abc004/c/main.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+
+using ll = long long int;
+using ul = unsigned long long int;
+const ll LL_MAX (1LL<<60);
+const ll MOD (1000000007);
+
+#define rep(i,s,e) for(ll i=(s); i<(e); i++) 
+
+using namespace std;
+
+void printv(vector<ll>& v)
+{
+    for (auto e: v)
+        cout << e;
+    cout << endl;
+}
+
+int main()
+{
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    ll n;
+    cin >>n;
+
+    //n = n % 6;
+
+    vector<ll> c(6);
+    iota(c.begin(),c.end(),1);
+    rep(i,0,n)
+    {
+        ll j = i%5;
+        swap(c[j],c[j+1]);
+        //printv(c);
+    }
+
+    printv(c);
+
+    return 0;
+}
---
+++ at_coder/abc004/c/run.sh
@@ -0,0 +1,7 @@
+
+#echo "" | ./a.out
+echo "1" | ./a.out
+echo
+echo "5" | ./a.out
+echo
+echo "100000000" | ./a.out
